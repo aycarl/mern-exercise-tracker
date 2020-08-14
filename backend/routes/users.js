345eb4d1918799e96c485bb7c850f264e457138b
Carl <carl.agbenyega@gmail.com>
2020-08-13 21:15:48 -0600

    added routes

---
+++ backend/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+// import user model
+let User = require('./../models/user.model');
+
+const router = express.Router();
+
+// GET - get all users
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err))
+});
+
+// POST - add a user
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+
+  const newUser = new User({username});
+
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,14 @@ const connection = mongoose.connection;
 
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
+
+// routes
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
