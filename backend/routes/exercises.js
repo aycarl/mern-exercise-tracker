c60fca4822901c865711fbdd766eafc0226c6382
Carl <carl.agbenyega@gmail.com>
2020-08-14 20:46:20 -0600

    Update exercises.js
    added new routes for update and delete

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,19 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 //import Exercise model
-let Exercise = require('./../models/exercise.model');
+let Exercise = require("./../models/exercise.model");
 
 const router = express.Router();
 
 // GET - get all exercises
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   Exercise.find()
-    .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then((exercises) => res.json(exercises))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// GET - get exercise by id
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // POST - add an exercise
-router.route('/add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
@@ -23,13 +30,36 @@ router.route('/add').post((req, res) => {
     username,
     description,
     duration,
-    date
+    date,
   });
 
-  newExercise.save()
-    .then(() => res.json('Exercise added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  newExercise
+    .save()
+    .then(() => res.json("Exercise added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
+// DELETE - remove an exercise by id
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted!"))
+    .catch((er) => res.status(400).json("Error: " + err));
+});
+
+// POST - update an exercise
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = req.body.duration;
+      exercise.date = Date.parse(req.body.date);
+      exercise
+        .save()
+        .then(() => res.json("Exercise updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
