3706fd0fddadb01283266a88c155e3cefbb06c30
Carl <carl.agbenyega@gmail.com>
2020-08-15 18:41:22 -0600

    added ui components for creating and listing exercises

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -9,24 +9,21 @@ import "react-datepicker/dist/react-datepicker.css";
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-class CreateExercise extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: "",
-      description: "",
-      duration: 0,
-      date: new Date(),
-      users: [],
-    };
-  }
+class CreateExerciseForm extends Component {
+  state = {
+    username: "",
+    description: "",
+    duration: 0,
+    date: new Date(),
+    users: [],
+  };
 
   componentDidMount() {
     axios.get("http://localhost:5000/users").then((response) => {
       if (response.data.length > 0) {
         this.setState({
           users: response.data.map((user) => user.username),
+          username: response.data[0].username,
         });
       }
     });
@@ -55,69 +52,72 @@ class CreateExercise extends Component {
       date,
     };
 
-    console.log(exercise);
-
     axios
-      .post("http://localhost:5000/exercises/add", exercise)
+      .post('http://localhost:5000/exercises/add', exercise)
       .then((res) => console.log(res.data));
-
-    setTimeout(() => {
-      window.location = "/";
-    }, 3000);
-    //window.location = "/";
   };
 
   render() {
     const { username, description, duration, date, users } = this.state;
 
+    console.log(username);
+
     return (
-      <div className="page-content">
-        <h4>Create New Exercise Log</h4>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group controlId="usernameControl">
-            <Form.Label>username</Form.Label>
-            <Form.Control as="select" name="username" defaultValue={username}>
-              {users.map((user) => (
-                <option key={user} value={user}>
-                  {user}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group controlId="descriptionControl">
-            <Form.Label>description</Form.Label>
-            <Form.Control
-              type="text"
-              name="description"
-              value={description}
-              placeholder="enter type of exercise"
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group controlId="durationControl">
-            <Form.Label>duration</Form.Label>
-            <Form.Control
-              type="text"
-              name="duration"
-              value={duration}
-              placeholder="enter exercise duration"
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group controlId="dateControl">
-            <Form.Label>date</Form.Label>
-            <br />
-            <DatePicker selected={date} onChange={this.handleDateChange} />
-          </Form.Group>
-          <Form.Group>
-            <Button type="submit" block>
-              Create Exercise Log
-            </Button>
-          </Form.Group>
-        </Form>
-      </div>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId="usernameControl">
+          <Form.Label>username</Form.Label>
+          <Form.Control as="select" name="username" defaultValue={username}>
+            {users.map((user) => (
+              <option key={user} value={user}>
+                {user}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="descriptionControl">
+          <Form.Label>description</Form.Label>
+          <Form.Control
+            type="text"
+            name="description"
+            value={description}
+            required
+            placeholder="enter type of exercise"
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="durationControl">
+          <Form.Label>duration</Form.Label>
+          <Form.Control
+            type="text"
+            name="duration"
+            required
+            value={duration}
+            placeholder="enter exercise duration"
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="dateControl">
+          <Form.Label>date</Form.Label>
+          <br />
+          <DatePicker selected={date} onChange={this.handleDateChange} />
+        </Form.Group>
+        <Form.Group>
+          <Button type="submit" block>
+            Create Exercise Log
+          </Button>
+        </Form.Group>
+      </Form>
     );
   }
 }
+
+const CreateExercise = () => {
+  return (
+    <div className="page-content">
+      <h4>Create New Exercise Log</h4>
+      <CreateExerciseForm />
+    </div>
+  );
+};
 
 export default CreateExercise;
--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import {} from 'react-bootstrap';
+import { Table, Button } from "react-bootstrap";
 
-const ExerciserList = () => {
-  return (
-    <div className="page-content">
-      <h4>List of Exercises:</h4>
-    </div>
-  )
-};
+class ExerciserList extends Component {
+  state = {
+    exercises: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/exercises").then((response) => {
+      if (response.data) {
+        this.setState({
+          exercises: response.data,
+        });
+      }
+    });
+  }
+
+  deleteExercise = (id) => {
+    axios.delete(`http://localhost:5000/exercises/${id}`)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      exercises: this.state.exercises.filter(exercise => exercise._id !== id)
+    });
+  }
+
+  render() {
+    const { exercises } = this.state;
+    
+    return (
+      <div className="page-content">
+        <h4>List of Exercises:</h4>
+        {exercises ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>description</th>
+                <th>duration</th>
+                <th>actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {exercises.map((exercise) => (
+                <tr key={exercise._id}>
+                  <td>{exercise.username}</td>
+                  <td>{exercise.description}</td>
+                  <td>{exercise.duration}</td>
+                  <td>
+                    <Link to={`/edit/${exercise._id}`}>
+                      <Button variant="outline-primary">Edit</Button>
+                    </Link>
+                    {" "}
+                    <Button
+                      variant="outline-danger"
+                      onClick={() => {this.deleteExercise(exercise._id)}}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          "No Exercises logged"
+        )}
+      </div>
+    );
+  }
+}
 
 export default ExerciserList;
